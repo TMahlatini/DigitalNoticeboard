0d8dd10f2fd08e7b2110291f8fb185a595547344
TMahlatini <mahlatit@whitman.edu>
2024-10-18 11:15:27 -0700

    commented out validations and :confirmable to test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :timeoutable
+         :recoverable, :rememberable, :validatable, :timeoutable #, :confirmable
 
-  validates :email, format: {with: /\A[\w+\-.]+@whitman\.edu\z/, message: "must be a Whitman College email address" }
+  #validates :email, format: {with: /\A[\w+\-.]+@whitman\.edu\z/, message: "must be a Whitman College email address" }
 
   has_many :sticker_notes, dependent: :destroy
 
