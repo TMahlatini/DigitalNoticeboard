f6e154821e3e2aa04cb0f9009ad841791652406f
TMahlatini <mahlatit@whitman.edu>
2024-10-18 11:32:02 -0700

    fixing some display issue

--- app/controllers/noticeboard_controller.rb
+++ app/controllers/noticeboard_controller.rb
@@ -1,5 +1,5 @@
 class NoticeboardController < ApplicationController
-  before_action :authenticate_user! # this line is used to make sure that only logged in users can access the noticeboard page
+  before_action :authenticate_user!, except: [:index] # this line is used to make sure that only logged in users can access the noticeboard page
 
   def index
     @sticker_notes = StickerNote.all.order(created_at: :desc)  #retrieves all the sticker notes from the database and orders them by the time they were created
