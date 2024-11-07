d8b583df017b31b5199972f8517bf145dbca43e9
TMahlatini <mahlatit@whitman.edu>
2024-11-07 14:00:05 -0800

    added comfirmable validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeoutable #, :confirmable
+         :recoverable, :rememberable, :validatable, :timeoutable, :confirmable
 
   validates :email, format: {with: /\A[\w+\-.]+@whitman\.edu\z/, message: "must be a Whitman College email address" }
 
